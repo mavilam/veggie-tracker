715cc28e82d1b17e4ffdb295bba98568d05a2610
Miguel Ávila Monroy <mavilam@evobanco.com>
2024-02-16 17:22:02 +0100

    group by type of veggie

--- src/components/VeggiesList.vue
+++ src/components/VeggiesList.vue
@@ -2,15 +2,21 @@
   <div class="text-veggie-800 mb-20">
     <SearchBar @search="filterVeggies" />
     <Counter :count="clickedIds.size" />
-    <div class="m-4 grid grid-cols-2 sm:grid-cols-3 gap-4">
-      <Veggie 
-        v-for="veggie in filteredVeggies" 
-        :key="veggie.id" 
-        :name="veggie.name"
-        :id="veggie.id"
-        :is-clicked="clickedIds.has(veggie.id)"
-        @clicked="handleVeggieClick"
-      />
+    <hr class="w-48 h-0.5 mx-auto my-4 bg-veggie-700 rounded md:my-10">
+    <div class="m-4">
+      <div class="m-2" v-for="(veggies, type) in groupedVeggies" :key="type">
+        <h3 class="text-l font-bold m-2 mt-4">{{ type }}:</h3>
+        <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
+          <Veggie 
+            v-for="veggie in veggies" 
+            :key="veggie.id" 
+            :name="veggie.name"
+            :id="veggie.id"
+            :is-clicked="clickedIds.has(veggie.id)"
+            @clicked="handleVeggieClick"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +48,16 @@ export default {
     };
   },
   computed: {
+    groupedVeggies() {
+      const group = {};
+      this.filteredVeggies.forEach((veggie) => {
+        if (!group[veggie.type]) {
+          group[veggie.type] = [];
+        }
+        group[veggie.type].push(veggie);
+      });
+      return group;
+    },
     filteredVeggies() {
       return this.veggies.filter(veggie =>
         veggie.name.toLowerCase().includes(this.searchQuery.toLowerCase())
